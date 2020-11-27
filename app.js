53e2a1d5e5e410a91721b07d83dff1cfaae66fb4
Victor Henrique <victorhrvs@gmail.com>
2020-11-27 11:10:01 -0300

    Adicionado o CRUD do Cadastro Socioeconômico

--- app.js
+++ app.js
@@ -44,17 +44,7 @@ app.get('/',(req, res) => {
 });
 
 
-app.get('/socioeconomico',(req, res) => {
-    // res.send('CRUD Operation using NodeJS / ExpressJS / MySQL');
-    let sql = "SELECT * FROM `socioeconomico`";
-    let query = connection.query(sql, (err, rows) => {
-        if(err) throw err;
-        res.render('socioeconomico_index', {
-            title : 'Cadastro Socioeconômico',
-            cadastros : rows
-        });
-    });
-});
+
 
 /////////////////////////////
 //Rotas para a Tabela Local//
@@ -128,6 +118,92 @@ app.get('/local_delete/:idlocal',(req, res) => {
             }
         } else {
             res.redirect('/local');
+        }
+
+    });
+});
+
+//////////////////////////////////////////////////
+//Rotas para a Tabela de Cadastro Socioeconômico//
+//////////////////////////////////////////////////
+
+// Read
+app.get('/socioeconomico',(req, res) => {
+    let sql = "SELECT * FROM `socioeconomico`";
+    let query = connection.query(sql, (err, rows) => {
+        if(err) throw err;
+        res.render('socioeconomico_index', {
+            title : 'Cadastro Socioeconômico',
+            cadastros : rows
+        });
+    });
+});
+
+// Create
+app.get('/socioeconomico_add',(req, res) => {
+    res.render('socioeconomico_add', {
+        title : 'Adicionar Cadastro Socioecônomico'
+    });
+});
+
+
+app.post('/socioeconomico_save',(req, res) => { 
+    var pbf = (!req.body.programa_bolsa_familia) ? 0 : 1;
+    var peti = (!req.body.programa_de_erradicacao_do_trabalho_infantil) ? 0 : 1;
+    var ppa = (!req.body.programa_projovem_adolescente) ? 0 : 1;
+
+    let data = {numero_de_membros_da_familia: req.body.numero_de_membros_da_familia, renda_per_capita: req.body.renda_per_capita, programa_bolsa_familia: pbf, programa_de_erradicacao_do_trabalho_infantil: peti, programa_projovem_adolescente: ppa};
+    let sql = "INSERT INTO `socioeconomico` SET ?";
+    let query = connection.query(sql, data,(err, results) => {
+        if(err) throw err;
+        res.redirect('/socioeconomico');
+    });
+});
+
+// Update
+app.get('/socioeconomico_edit/:id_socioeconomico',(req, res) => {
+    const idsocioeconomico = req.params.id_socioeconomico;
+    let sql = `Select * from notebooks_para_todos.socioeconomico where id_socioeconomico = ${idsocioeconomico}`;
+    let query = connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.render('socioeconomico_edit', {
+            title : 'Editar cadastro socioeconomico',
+            socioeconomico : result[0]
+        });
+        //console.log(result[0].programa_bolsa_familia == 1);
+    });
+});
+
+app.post('/socioeconomico_update',(req, res) => {
+    var pbf = (!req.body.programa_bolsa_familia) ? 0 : 1;
+    var peti = (!req.body.programa_de_erradicacao_do_trabalho_infantil) ? 0 : 1;
+    var ppa = (!req.body.programa_projovem_adolescente) ? 0 : 1;
+
+    const id_socioeconomico = req.body.id_socioeconomico;
+    let sql = "update notebooks_para_todos.socioeconomico SET numero_de_membros_da_familia='"+req.body.numero_de_membros_da_familia+"',  renda_per_capita='"+req.body.renda_per_capita+"',  programa_bolsa_familia='"+pbf+"', programa_de_erradicacao_do_trabalho_infantil='"+peti+"', programa_projovem_adolescente='"+ppa+"' where id_socioeconomico ="+id_socioeconomico;
+    let query = connection.query(sql,(err, results) => {
+        if(err) throw err;
+        res.redirect('/socioeconomico');
+    });
+});
+
+// Delete
+app.get('/socioeconomico_delete/:id_socioeconomico',(req, res) => {
+    const id_socioeconomico = req.params.id_socioeconomico;
+    let sql = `DELETE from notebooks_para_todos.socioeconomico where id_socioeconomico = ${id_socioeconomico}`;
+    let query = connection.query(sql,(err, result) => {
+        if(err) {
+            if(err.errno == 1451){
+                console.log("Cadastrado Socioeconômico está sendo utilizado por um ou mais Alunos")
+                res.render('alert', {
+                    title : 'Não é possível deletar',
+                    msg: "Cadastrado Socioeconômico está sendo utilizado por um ou mais Alunos"
+                });
+            } else {
+                throw err;
+            }
+        } else {
+            res.redirect('/socioeconomico');
         }
 
     });
